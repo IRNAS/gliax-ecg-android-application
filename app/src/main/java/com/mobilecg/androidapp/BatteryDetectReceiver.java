f36dbbb389619df5428ac0e3c42c00f11638b40e
Vid Rajtmajer <vid.rajtmajer@gmail.com>
2019-12-09 23:01:18 +0100

    minor bug fix

--- app/src/main/java/com/mobilecg/androidapp/BatteryDetectReceiver.java
+++ app/src/main/java/com/mobilecg/androidapp/BatteryDetectReceiver.java
@@ -31,7 +31,6 @@ public class BatteryDetectReceiver extends BroadcastReceiver {
         IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         Intent batteryStatus = context.registerReceiver(null, ifilter);
         int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean bCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
-        return bCharging;
+        return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
     }
 }
